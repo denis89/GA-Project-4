15c9b51a9b2fc6316eb9e61a8314867c24fa3331
Denis Doan <dedoin08@yahoo.de>
2015-10-26 11:53:36 +0000

    Show done.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,25 @@
 class AdsController < ApplicationController
+   def show
+    @ads = Ads.find(params[:id])
+    # Find a specific article
+  end
+
   def new 
   end
+
+  def create 
+   #create action to use the new Article model to save the data in the database.
+   @ads = Ads.new(ads_params)
+   @ads.save
+  redirect_to @ads
+  end
+
+  private
+  def ads_params
+    params.require(:ads).permit(:title, :text)
+  end
+  
+
+
+
 end
